a26c0dff336f576ce82de17dd4bd7fe06ef3ff42
zzb1019424692 <1019424692@qq.com>
2019-12-09 21:14:56 +0800

    work15

---
+++ work15/index.js
@@ -0,0 +1,31 @@
+ShopCart('cart',[{
+    name: '郑泽宏 (渣男) ',
+
+    price: 5.5 ,
+    num: 2
+},
+{
+    name: '马维利 (马贼) ',
+
+    price: 66.6,
+    num: 2
+},
+{
+    name: '黄重伟 (赖瓜子)',
+
+    price: 77.7,
+    num: 2
+},
+{
+    name: '李荆 (骚鸡公) ',
+
+    price: 88.8,
+    num: 2
+},
+{
+    name: '陈聪 (嫩仔) ',
+
+    price: 99.9,
+    num: 2
+}
+]);
